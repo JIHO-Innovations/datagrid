a657b92f2646ac723830cc2ac191d845b9b81859
Martin Prokopič <martin@prokopic.net>
2026-02-05 16:35:57 +0100

    Validate ColumnStatus value before calling onChange

--- src/Column/ColumnStatus.php
+++ src/Column/ColumnStatus.php
@@ -25,6 +25,8 @@ class ColumnStatus extends Column
 
 	protected array $options = [];
 
+	public bool $nullable = false;
+
 	public function __construct(
 		Datagrid $grid,
 		string $key,
@@ -49,10 +51,17 @@ class ColumnStatus extends Column
 		return $this->options;
 	}
 
+	public function setNullable(bool $nullable = true): static
+	{
+		$this->nullable = $nullable;
+		return $this;
+	}
+
 	/**
 	 * @throws DatagridColumnStatusException
+	 * @return ($throw is true ? Option : ?Option)
 	 */
-	public function getOption(mixed $value): Option
+	public function getOption(mixed $value, bool $throw = true): ?Option
 	{
 		foreach ($this->options as $option) {
 			if ($option->getValue() === $value) {
@@ -60,7 +69,10 @@ class ColumnStatus extends Column
 			}
 		}
 
-		throw new DatagridColumnStatusException(sprintf('Option [%s] does not exist', $value));
+		if($throw)
+			throw new DatagridColumnStatusException(sprintf('Option [%s] does not exist', $value));
+
+		return null;
 	}
 
 	public function getColumn(): string
@@ -73,13 +85,10 @@ class ColumnStatus extends Column
 	 */
 	public function getCurrentOption(Row $row): ?Option
 	{
-		foreach ($this->getOptions() as $option) {
-			if ($option->getValue() === $row->getValue($this->getColumn())) {
-				return $option;
-			}
-		}
-
-		return null;
+		$value = $row->getValue($this->getColumn());
+		if($value === null && $this->nullable)
+			$value = "";
+		return $this->getOption($value, false);
 	}
 
 	/**
